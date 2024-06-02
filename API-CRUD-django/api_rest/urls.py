6d6fcb129151efee3572a6d17e1288a001967f92
unknown <nicolas.raony06@gmail.com>
2024-06-02 15:35:45 -0300

    Criando url data/; e view user_manager com apenas método GET para receber url com parâmetro user

--- API-CRUD-django/api_rest/urls.py
+++ API-CRUD-django/api_rest/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.get_users, name='get_users'),
-    path('user/<str:nickname>', views.get_user_by_nickname, name='get_user_by_nickname')
+    path('user/<str:nickname>', views.get_user_by_nickname, name='get_user_by_nickname'),
+    path('data/', views.user_manager, name='user_manager'),
 ]
--- API-CRUD-django/api_rest/views.py
+++ API-CRUD-django/api_rest/views.py
@@ -32,3 +32,25 @@ def get_user_by_nickname(request, nickname):
         return Response(serializer.data)
         
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_manager(request):
+    if request.method == 'GET':  
+        try:                                                           #Tenta com a url /?user=algumvalor
+            if request.GET['user']:                                    #Checar se request.GET['user'] não está recebendo um valor None
+                nickname = request.GET['user']
+ 
+                try:
+                    user = User.objects.get(pk=nickname)
+                except:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+                
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+            
+            else:                                                      #Caso request.GET['user'] receba None, retorna um erro http 400
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            
+        except:                                                        #Caso a url seja passada com um parâmetro errado, retorna http 400
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
