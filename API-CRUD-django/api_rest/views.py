8602e5c1bce1e686c2215d987269cd2e736c09fe
unknown <nicolas.raony06@gmail.com>
2024-06-08 12:20:53 -0300

    Add método PUT na view get_user_by_nick, para fazer alteração via link direto passando o Nickname via url

--- API-CRUD-django/api_rest/views.py
+++ API-CRUD-django/api_rest/views.py
@@ -20,18 +20,25 @@ def get_users(request):
     
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def get_user_by_nickname(request, nickname):
+    try:
+        user = User.objects.get(pk=nickname)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
     if request.method == 'GET':
-        try:
-            user = User.objects.get(pk=nickname)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
         serializer = UserSerializer(user)
         return Response(serializer.data)
-        
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PUT':
+        serializer = UserSerializer(user, request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def user_manager(request):
